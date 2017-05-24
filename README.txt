9395b2baeadfdb2ac16c9fe02f50962f3695d52a
Lakshman Joshi <lakshman@spliceglobal.com>
2017-05-24 07:56:48 +0530

    commit module

---
+++ README.txt
@@ -0,0 +1,2 @@
+#This module I have developed my own knowlage and take some references Drupal.org and Drupal Core modules
+#Total Time Taken 4 hours.
---
+++ src/Controller/AxelerantChallengeController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\axelerant_challenge\Controller;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\NodeInterface;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Controller routines for AxelerantChallenge routes.
+ */
+class AxelerantChallengeController extends ControllerBase {
+
+  /**
+   * function return the node data
+   * @param $api_key strin type
+   * @param NodeInterface $node
+   * @return array
+   */
+
+  public function content($api_key, NodeInterface $node) {
+    $output = array();
+    $output = array('node' => $node->toArray());
+    return new JsonResponse($output);
+  }
+
+  /**
+   *
+   * Access callback function for routing menu
+   * @param $api_key String type
+   * @param NodeInterface $node object node
+   *
+   * @return \Drupal\Core\Access\AccessResultAllowed|\Drupal\Core\Access\AccessResultForbidden
+   */
+  public function axelerantNodeAccess($api_key, NodeInterface $node) {
+    if (\Drupal::configFactory()->getEditable('system.site')->get('siteapikey') == $api_key && $node->getType() == 'page') {
+      return AccessResult::allowed();
+    }
+    else {
+      return AccessResult::forbidden();
+    }
+
+
+ }
+}
